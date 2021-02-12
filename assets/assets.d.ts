f678a449062a3642a5b1211a37fbf12bc740a216
Yoni <firroloni.yoni@gmail.com>
2021-02-12 01:24:41 +0100

    fix lint

--- assets/assets.d.ts
+++ assets/assets.d.ts
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
   const content: any;
   export default content;
 }
 
 declare module '*.png' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
 
 declare module '*.jpg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
